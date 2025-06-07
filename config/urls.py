e3af4ad04e6a9579b75ed22a1289292432a9be1b
babadu14 <andriamanagadze7@gmail.com>
2025-06-08 01:07:37 +0400

    added users registration logic

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,34 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='API for Resume-AI',
+        default_version='v1',
+        description='Resume-AI project'
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny]
+)
+
+
+
 
 urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('', include('resume.urls')),
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-ui'),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- resume/models.py
+++ resume/models.py
@@ -6,3 +6,4 @@ class ResumeFeedback(models.Model):
     resume_file = models.FileField(upload_to='media/', validators=[validate_file_size])
     feedback = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey("users.User", related_name='resumes', on_delete=models.CASCADE)
